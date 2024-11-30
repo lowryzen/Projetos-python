8e245e34dfcfdff75f738123fe622eeff343a0a8
lowryzen <lowhack@tutamail.com>
2024-11-30 18:00:01 -0300

    Update básico

--- Jogo_da_forca.py
+++ Jogo_da_forca.py
@@ -1,23 +1,22 @@
-#jogo da forca
+# jogo da forca
 
 def jogo_forca():
     import random
 
-    palavra = ("Banana", "Sapato","Tesoura", "Macaco", "Mousse", "Tiranossauro rex")
+    palavra = ("Banana", "Sapato", "Tesoura", "Macaco", "Mousse", "Tiranossauro rex")
     aleatorio = random.choice(palavra)
 
     pergunta = str(input("Deseja jogar?: ")).capitalize()
-    resposta= str(input("321"))
 
-    if pergunta == ("Sim"): 
+    if pergunta == "Sim":
         for cadaletra in aleatorio:
-            print (cadaletra.replace(cadaletra, "_"), end=" ")
-            
-    else:
-        None
+            print(cadaletra.replace(cadaletra, "_"), end=" ")
+        resposta = str(input("insira uma letra: "))
+        if resposta == aleatorio:
+            print ("Nooooosa")
 
+    else:
+        print ("Tá bom.")
 
 if __name__ == "__main__":
     jogo_forca()
-    
-    
